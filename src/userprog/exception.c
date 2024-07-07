36b8476e4fe178c6f2e2ce604540716f7f17bf46
gyxx-xc <xdll_xc@outlook.com>
2024-07-07 18:13:00 +0800

    page fault done

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -139,6 +139,11 @@ static void page_fault(struct intr_frame* f) {
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
+  if (!user){
+    f->eip = (void*) f->eax;
+    f->eax = -1;
+    return;
+  }
   printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
          not_present ? "not present" : "rights violation", write ? "writing" : "reading",
          user ? "user" : "kernel");
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,10 +8,23 @@
 #include "filesys/filesys.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
+#include "threads/malloc.h"
+#include "threads/vaddr.h"
 
 struct semaphore file_lock;
 
 static void syscall_handler(struct intr_frame*);
+
+static bool put_user (uint8_t *, uint8_t );
+static int get_user (const uint8_t *);
+
+static bool check_user_string (const char*);
+static bool check_user_buff(const uint8_t*, off_t);
+static void put_user_buff(const uint8_t*, uint8_t*, off_t);
+
+static bool check_args(uint32_t*, int);
+
+static void kill_process(void);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
@@ -25,142 +38,240 @@ static void syscall_handler(struct intr_frame* f) {
    * include it in your final submission.
    */
 
+  if (get_user((uint8_t*)args) == -1)
+    kill_process();
+  if (get_user((uint8_t*)args+1) == -1)
+    kill_process();
+  if (get_user((uint8_t*)args+2) == -1)
+    kill_process();
+  if (get_user((uint8_t*)args+3) == -1)
+    kill_process();
+
   /* printf("System call number: %d\n", args[0]); */
-  int fd;struct file *file;
+  int fd;
+  struct file *file;
   switch (args[0]){
   case SYS_HALT: // 0
     shutdown_power_off();
-    break;
+    return;
 
   case SYS_EXIT: // 1
+    check_args(args, 1);
     f->eax = args[1];
     printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
     process_exit(args[1]);
-    break;
+    return;
 
   case SYS_EXEC: // 2
+    check_args(args, 1);
+    check_user_string((void*)args[1]);
     f->eax = process_execute((const char*)args[1]);
-    break;
+    return;
 
   case SYS_WAIT: // 3
+    check_args(args, 1);
     f->eax = process_wait(args[1]);
-    break;
+    return;
 
   case SYS_CREATE: // 4
+    check_args(args, 1);
+    check_user_string((void*)args[1]);
     sema_down(&file_lock);
     f->eax = filesys_create((char*)args[1], args[2]);
     sema_up(&file_lock);
-    break;
+    return;
 
   case SYS_REMOVE: // 5
+    check_args(args, 1);
+    check_user_string((void*)args[1]);
     sema_down(&file_lock);
     f->eax = filesys_remove((char*)args[1]);
     sema_up(&file_lock);
-    break;
+    return;
 
   case SYS_OPEN: // 6
+    check_args(args, 1);
+    check_user_string((void*)args[1]);
     sema_down(&file_lock);
     file = filesys_open((char*)args[1]);
     sema_up(&file_lock);
     if (file == NULL) {
       f->eax = -1;
-      break;
+      return;
     }
     fd = ++ thread_current()->pcb->fd_count; // Okay, I know it's shit
     struct fdtable *fdt = &thread_current()->pcb->fdt[fd];
     fdt->file_pointer = file;
     fdt->valid = true;
     f->eax = fd;
-    break;
+    return;
 
   case SYS_FILESIZE: // 7
+    check_args(args, 1);
     fd = args[1];
     file = get_file(thread_current()->pcb, fd);
     if (file == NULL) {
       f->eax = -1;
-      break;
+      return;
     }
     sema_down(&file_lock);
     f->eax = file_length(file);
     sema_up(&file_lock);
-    break;
+    return;
 
   case SYS_READ: // 8
+    check_args(args, 3);
     fd = args[1];
+    uint8_t* buffer = malloc(sizeof(uint8_t)*args[3]);
     if (fd == 0) {
-      char* buffers = (void*)args[2];
       for (unsigned int i = 0; i < args[3]; i ++) {
-        *(buffers ++) = input_getc();
+        *(buffer ++) = input_getc();
       }
       f->eax = args[3];
-      break;
     } else {
       file = get_file(thread_current()->pcb, fd);
       if (file == NULL) {
         f->eax = -1;
-        break;
+        return;
       }
       sema_down(&file_lock);
-      f->eax = file_read(file, (void*)args[2], args[3]);
+      f->eax = file_read(file, buffer, args[3]);
       sema_up(&file_lock);
-      break;
     }
+    put_user_buff(buffer, (uint8_t*)args[2], f->eax);
+    return;
 
   case SYS_WRITE: // 9
+    check_args(args, 3);
     fd = args[1];
+    check_user_buff((void*)args[2], args[3]);
     if (fd == 1) {
       putbuf((void*)args[2], args[3]);
       f->eax = args[3];
-      break;
+      return;
     } else {
       file = get_file(thread_current()->pcb, fd);
       if (file == NULL) {
         f->eax = -1;
-        break;
+        return;
       }
       sema_down(&file_lock);
       f->eax = file_write(file, (void*)args[2], args[3]);
       sema_up(&file_lock);
-      break;
+      return;
     }
 
   case SYS_SEEK: // 10
+    check_args(args, 2);
     fd = args[1];
     file = get_file(thread_current()->pcb, fd);
     if (file == NULL) {
       f->eax = -1;
-      break;
+      return;
     }
     sema_down(&file_lock);
     file_seek(file, args[2]);
     sema_up(&file_lock);
-    break;
+    return;
 
   case SYS_TELL: // 11
+    check_args(args, 1);
     fd = args[1];
     file = get_file(thread_current()->pcb, fd);
     if (file == NULL) {
       f->eax = -1;
-      break;
+      return;
     }
     sema_down(&file_lock);
     f->eax = file_tell(file);
     sema_up(&file_lock);
-    break;
+    return;
 
   case SYS_CLOSE: // 12
+    check_args(args, 1);
     fd = args[1];
     file = get_file(thread_current()->pcb, fd);
     if (file == NULL)
-      break;
+      return;
     thread_current()->pcb->fdt[fd].valid = false;
     sema_down(&file_lock);
     file_close(file);
     sema_up(&file_lock);
-    break;
+    return;
 
   case SYS_PRACTICE: // who cares this number...
+    check_args(args, 1);
     f->eax = args[1] + 1;
-    break;
+    return;
   }
+
+  // other case including:
+  // args not found
+  kill_process();
+}
+
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful,
+   -1 if a segfault occurred. */
+static int get_user (const uint8_t *uaddr) {
+  if ((void*)uaddr >= PHYS_BASE) return -1;
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful,
+   false if a segfault occurred. */
+static bool put_user (uint8_t *udst, uint8_t byte) {
+  if ((void*)udst >= PHYS_BASE) return false;
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+
+static bool check_user_string (const char* input) {
+  int i = 0;
+  while (true) {
+    i = get_user((void*)input++);
+    if (i == -1) kill_process();
+    if ((char)i == '\0') return true;
+  }
+}
+
+static bool check_user_buff(const uint8_t* input, off_t size){
+  for (int i = 0; i < size; i ++)
+    if (get_user(input++) == -1)
+      kill_process();
+  return true;
+}
+
+static void put_user_buff(const uint8_t* output, uint8_t* buff, off_t size){
+  for (int i = 0; i < size; i ++)
+    if (!put_user(buff++, *(output++)))
+      kill_process();
+}
+
+static bool check_args(uint32_t* args, int argc) {
+  for (int i = 0; i < argc; i ++){
+    if (get_user((void*)(++args)) == -1)
+      kill_process();
+    if (get_user((uint8_t*)args+1) == -1)
+      kill_process();
+    if (get_user((uint8_t*)args+2) == -1)
+      kill_process();
+    if (get_user((uint8_t*)args+3) == -1)
+      kill_process();
+  }
+  return true;
+}
+
+static void kill_process(void){
+  printf("%s: exit(-1)\n", thread_current()->pcb->process_name);
+  process_exit(-1);
+  NOT_REACHED();
 }
